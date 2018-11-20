cc342797c462ad3963a216291df7b886baf2e2cb
y-wan <908244927@qq.com>
2018-11-20 08:11:59 +0800

    Add online/offline badge; add web learning info to popup page

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,68 @@
-if (localStorage.getItem('username') !== null) {
+getLoginPopup = function() {
   document.getElementById('login-panel').style.display = "none";
   document.getElementById('logout-panel').style.display = "block";
   document.getElementById('username-online').innerText = localStorage.getItem('username');
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      let courseInfoList = parseCoursePage(xhr.responseText);
+      let prevPanel = document.getElementById("info-panel");
+      if (prevPanel !== null) {
+        prevPanel.parentNode.removeChild(prevPanel);
+      }
+      let infoPanel = document.createElement("div");
+      infoPanel.id = "info-panel";
+      infoPanel.classList.add("box");
+      for (let [courseName, courseInfo] of Object.entries(courseInfoList)) {
+        let [message, url] = updateSingleCourse(courseName, courseInfo);
+        if (message === "") continue;
+        let courseNode = document.createElement("article");
+        courseNode.classList.add("media");
+        let hyperlink = document.createElement("a");
+        hyperlink.href = url;
+        let content = document.createElement("div");
+        content.classList.add("content");
+        let titleNode = document.createElement("strong");
+        titleNode.appendChild(document.createTextNode(courseName));
+        content.appendChild(titleNode);
+        let messageNode = document.createElement("p");
+        let smallNode = document.createElement("small");
+        smallNode.appendChild(document.createTextNode(message));
+        messageNode.appendChild(smallNode);
+        content.appendChild(messageNode);
+        hyperlink.appendChild(content);
+        courseNode.appendChild(hyperlink);
+        infoPanel.appendChild(courseNode);
+      }
+      document.getElementById("logout-panel").insertBefore(infoPanel, document.getElementById("username-online").nextSibling);
+    }
+  }
+  xhr.open('GET', 'http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/MyCourse.jsp', true);
+  xhr.send();
+};
+
+login = function() {
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      // login failure
+      if (xhr.responseText.indexOf('alert') > -1) {
+        window.alert('用户名或密码错误');
+        setBadge(online=false);
+        return;
+      }
+      // login success
+      setBadge(online=true);
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+      getLoginPopup();
+    }
+  }
+  xhr.open('POST', 'https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp', true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("userid=" + encodeURIComponent(username) + "&userpass=" + encodeURIComponent(password) + "&submit1=%E7%99%BB%E5%BD%95");
 }
 
 // login
@@ -9,28 +70,7 @@ document.getElementById('login').onclick = () => {
   let username = document.getElementById('username').value;
   let password = document.getElementById('password').value;
   if (username.trim() === "" || password === "") return;
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-      // login failure
-      if (xhr.responseText.indexOf('alert') > -1) {
-        window.alert('用户名或密码错误');
-        return;
-      }
-      // login success
-      localStorage.setItem('username', document.getElementById('username').value);
-      localStorage.setItem('password', document.getElementById('password').value);
-      window.alert('用户' + localStorage.username + '登陆成功');
-      document.getElementById('login-panel').style.display = "none";
-      document.getElementById('logout-panel').style.display = "block";
-      document.getElementById('username-online').innerText = localStorage.username;
-      // getCourseInfo();
-      browser.runtime.getBackgroundPage(page => {page.getCourseInfo()});
-    }
-  }
-  xhr.open('POST', 'https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp', true);
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send("userid=" + encodeURIComponent(username) + "&userpass=" + encodeURIComponent(password) + "&submit1=%E7%99%BB%E5%BD%95");
+  login();
 };
 
 // logout
@@ -42,5 +82,11 @@ document.getElementById('logout').onclick = () => {
   document.getElementById('username-online').innerText = "";
   document.getElementById('login-panel').style.display = "block";
   document.getElementById('logout-panel').style.display = "none";
-  window.alert('用户' + username + '注销成功');
+  setBadge(online=false);
 };
+
+if (localStorage.getItem('username') !== null) {
+  document.getElementById('username').value = localStorage.getItem('username');
+  document.getElementById('password').value = localStorage.getItem('password');
+  login();
+}
---
+++ js/util.js
@@ -0,0 +1,78 @@
+let colorOnline = '#00C853';
+let textOnline = '√';
+let colorOffline = '#DD2C00';
+let textOffline = 'X';
+
+setBadge = function(online=true) {
+  if (online) {
+    browser.browserAction.setBadgeBackgroundColor({color: colorOnline});
+    browser.browserAction.setBadgeText({text: textOnline});
+  } else {
+    browser.browserAction.setBadgeBackgroundColor({color: colorOffline});
+    browser.browserAction.setBadgeText({text: textOffline});
+  }
+}
+
+generateMessage = function(message, number, type, sep='，') {
+  if (number <= 0) return message;
+  if (message !== "") message += sep;
+  return message + number.toString() + "个" + type;
+};
+
+getCourseId = function(url) {
+  if (url.indexOf('course_id') > -1) return url.split('=').slice(-1)[0];
+  else return url.split('/').slice(-1)[0];
+};
+
+parseCoursePage = function(page) {
+  parser = new DOMParser();
+  htmlDoc = parser.parseFromString(page, "text/html");
+  let courseTable = htmlDoc.getElementById("info_1");
+  if (courseTable === null) return;
+  let courseList = Array.from(courseTable.firstElementChild.children).slice(2);
+  let courseInfoList = {};
+  courseList.forEach(item => {
+    let url = item.children[0].children[1].href;
+    let courseUrl = url.indexOf('/MultiLanguage') > -1 ? "http://learn.tsinghua.edu.cn" + url.slice(url.indexOf('/MultiLanguage')) : url;
+    let courseName = item.children[0].children[1].text.split("(")[0].trim();
+    let numHomework = parseInt(item.children[1].children[0].textContent);
+    let numNotice = parseInt(item.children[2].children[0].textContent);
+    let numFile = parseInt(item.children[3].children[0].textContent);
+    courseInfoList[courseName] = { courseUrl, numHomework, numNotice, numFile };
+  });
+  return courseInfoList;
+}
+
+updateSingleCourse = function(courseName, courseInfo) {
+  let courseInfoOld = JSON.parse(localStorage.getItem(courseName));
+  localStorage.setItem(courseName, JSON.stringify(courseInfo));
+  let messagePopup = "";
+  let message = "";
+  messagePopup = generateMessage(messagePopup, courseInfo.numHomework, "未交作业", sep='\n');
+  messagePopup = generateMessage(messagePopup, courseInfo.numNotice, "未读公告", sep='\n');
+  messagePopup = generateMessage(messagePopup, courseInfo.numFile, "未读文件", sep='\n');
+  if (courseInfoOld !== null) {
+    let newHomework = courseInfo.numHomework - courseInfoOld.numHomework;
+    let newNotice = courseInfo.numNotice - courseInfoOld.numNotice;
+    let newFile = courseInfo.numFile - courseInfoOld.numFile;
+    message = generateMessage(message, newHomework, "新作业");
+    message = generateMessage(message, newNotice, "新公告");
+    message = generateMessage(message, newFile, "新文件");
+  } else {
+    let newNotice = courseInfo.numNotice;
+    let newFile = courseInfo.numFile;
+    message = generateMessage(message, newNotice, "新公告");
+    message = generateMessage(message, newFile, "新文件");
+  }
+  if (message !== "") {
+    browser.notifications.create(getCourseId(courseInfo.courseUrl), {
+      type: "basic",
+      iconUrl: "favicon.png",
+      title: courseName,
+      message: message,
+      contextMessage: "点击进入课程页面",
+      requireInteraction: false
+    });
+  }
+  return [messagePopup, courseInfo.courseUrl];
+}
